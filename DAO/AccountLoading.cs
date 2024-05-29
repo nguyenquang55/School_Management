ca0e5a982113d0b8bcda4d01b239cbfef0d24269
nguyenquang55 <160747632+nguyenquang55@users.noreply.github.com>
2024-05-29 21:08:37 +0700

    3
    33

---
+++ DAO/AccountLoading.cs
@@ -0,0 +1,188 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client.NativeInterop;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace test_app.DAO
+{
+    public class AccountLoading
+    {
+        private static AccountLoading instance;
+
+        public static AccountLoading Instance
+        {
+            get { if (instance == null) instance = new AccountLoading(); return instance; }
+            set { instance = value; }
+        }
+
+        private AccountLoading() { }
+
+        public bool Login(string username, string password)
+        {
+            return false;
+        }
+
+        public bool updateStudent(string username, string name, string sex, string birthdate, string idCard, string bornPlace, string ethnic, string hometown, string nationality, string religion, string phoneNumber, string email)
+        {
+
+
+            string query1 = $@"
+                 UPDATE dbo.StudentInfromation
+                 SET studentName = N'{name}',
+                     Sex = '{sex}',
+                     birth = '{birthdate}',
+                     IDCardNumber = {idCard},
+                     bornPlace = N'{bornPlace}',
+                     ethnic = N'{ethnic}',
+                     hometown = N'{hometown}',
+                     nationality = N'{nationality}',
+                     religion = N'{religion}',
+                     phoneNumber = {phoneNumber},
+                     email = '{email}'
+                 WHERE ID = '{username}'";
+
+
+            int result = DataProvider.Instance1.ExecuteNonQuery(query1);
+
+            return result > 0;
+
+        }
+
+        public bool updateNewStudent(string username, string name, string sex, string birthdate, string idCard, string bornPlace, string ethnic, string hometown, string nationality, string religion, string phoneNumber, string email)
+        {
+
+
+            string query1 = $@"
+                    INSERT INTO dbo.StudentInfromation
+                    (
+                        ID,
+                        studentName,
+                        Sex,
+                        birth,
+                        IDCardNumber,
+                        bornPlace,
+                        ethnic,
+                        hometown,
+                        nationality,
+                        religion,
+                        phoneNumber,
+                        email,
+                        majorID,
+                        departmentID
+                    )
+                    VALUES
+                    (
+                        {username},         -- ID - numeric(18, 0)
+                        N'{name}',       -- studentName - nvarchar(100)
+                        '{sex}',        -- Sex - varchar(10)
+                        ('{birthdate}'), -- birth - date
+                        {idCard},         -- IDCardNumber - numeric(18, 0)
+                        N'{bornPlace}',       -- bornPlace - nvarchar(200)
+                        N'{ethnic}',       -- ethnic - nvarchar(100)
+                        N'{hometown}',       -- hometown - nvarchar(100)
+                        N'{nationality}',       -- nationality - nvarchar(100)
+                        N'{religion}',       -- religion - nvarchar(100)
+                        {phoneNumber},      -- phoneNumber - numeric(20, 0)
+                        '{email}',      -- email - varchar(100)
+                        NULL,      -- majorID - varchar(10)
+                        NULL       -- departmentID - varchar(10)
+                    )";
+
+
+            int result = DataProvider.Instance1.ExecuteNonQuery(query1);
+
+            return result > 0;
+
+        }
+
+
+
+        public bool UpdateTeacher(string username, string name, string sex, string birthdate, string idCard, string bornPlace, string ethnic, string hometown, string nationality, string religion, string phoneNumber, string email)
+        {
+
+            string teacherQuery = $@"
+                UPDATE teacherinformations
+                SET
+                    teacherName = N'{name}',
+                    sex = N'{sex}',
+                    birth = '{birthdate}',
+                    IDCard = {idCard},
+                    bornPlace = N'{bornPlace}',
+                    ethnic = N'{ethnic}',
+                    hometown = N'{hometown}',
+                    nationality = N'{nationality}',
+                    religion = N'{religion}',
+                    phoneNumber = {phoneNumber},
+                    email = '{email}'
+                WHERE
+                    ID = '{username}'";
+
+            int result = DataProvider.Instance1.ExecuteNonQuery(teacherQuery);
+
+            return result > 0;
+        }
+
+        public bool UpdateNewTeacher(string username, string name, string sex, string birthdate, string idCard, string bornPlace, string ethnic, string hometown, string nationality, string religion, string phoneNumber, string email)
+        {
+            string newTeacherQuery = $@"
+                    INSERT INTO dbo.teacherinformations
+                    (
+                        ID,
+                        teacherName,
+                        sex,
+                        birth,
+                        IDCard,
+                        bornPlace,
+                        ethnic,
+                        hometown,
+                        nationality,
+                        religion,
+                        phoneNumber,
+                        email,
+                        departmentID,
+                        majorID
+                    )
+                    VALUES
+                    (
+                        '{username}',        -- ID - varchar(10)
+                        N'{name}',           -- teacherName - nvarchar(100)
+                        N'{sex}',            -- sex - nvarchar(10)
+                        '{birthdate}',       -- birth - date
+                        {idCard},            -- IDCard - numeric
+                        N'{bornPlace}',      -- bornPlace - nvarchar(100)
+                        N'{ethnic}',         -- ethnic - nvarchar(100)
+                        N'{hometown}',       -- hometown - nvarchar(100)
+                        N'{nationality}',    -- nationality - nvarchar(100)
+                        N'{religion}',       -- religion - nvarchar(100)
+                        {phoneNumber},       -- phoneNumber - numeric
+                        '{email}',           -- email - varchar(100)
+                        'NULL',              -- departmentID - varchar(10)
+                        'NULL'            -- majorID - varchar(10)
+                    )";
+
+            int result = DataProvider.Instance1.ExecuteNonQuery(newTeacherQuery);
+
+            return result > 0;
+        }
+
+        public bool updateStudentNewPassword(string username , string password)
+        {
+            string query = $"update StudentAccount\r\nset password='{password}'\r\nwhere username={username}";
+            int result = DataProvider.Instance1 .ExecuteNonQuery(query);
+            return result > 0;
+
+        }
+        public bool updateTeacherNewPasseord(string username , string password)
+        {
+            string query = $"update db_TeacherAccount\r\nset  userPassword='{password}'\r\nwhere userName='{username}'";
+            int result = DataProvider .Instance1 .ExecuteNonQuery(query); return result > 0;
+        }
+
+    }
+}
---
+++ DAO/DataProvider.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace test_app.DAO
+{
+    public  class DataProvider
+    {
+        private string connectionString = @"Data Source=DESKTOP-2LMB9L0\SQLEXPRESS;Initial Catalog=Student_management;Integrated Security=True;Trust Server Certificate=True;";
+
+        private static DataProvider instance;
+
+        public static DataProvider Instance1
+        {
+            get { if (instance == null) instance = new DataProvider(); return instance; }
+            private set { instance = value; }
+        }
+
+        public DataTable ExecuteQuery(string query )
+        {
+            DataTable data = new DataTable();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            adapter.Fill(data);
+                        }
+                    }
+                    connection.Close();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                
+                //MessageBox.Show($"SQL Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int data = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        // Add parameters to the command
+                        connection.Open();
+                        data = cmd.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (SqlException ex)
+            {
+                // Handle SQL exceptions
+                MessageBox.Show($"SQL Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Handle other potential exceptions
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
+            return data;
+        }
+
+
+        public object ExecuteScalar(string query)
+        {
+            object data = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+                        data = cmd.ExecuteScalar();
+                    }
+                    connection.Close();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                // Handle SQL exceptions
+                MessageBox.Show($"SQL Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Handle other potential exceptions
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
+            return data;
+        }
+    }
+}
